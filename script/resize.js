5a1f74b31bc8a0c111f56ef7deaad2dc6a99334b
Ramon Vieira <ramonvieira122333@gmail.com>
2022-09-02 15:57:31 -0300

    funcionalidade do header em dispositivos móveis e ajuste no conteudo main para melhor responsividade

--- script/resize.js
+++ script/resize.js
@@ -2,15 +2,13 @@ const contentMain = document.querySelector(".conteudo-main");
 const sectionObjetivos = document.querySelector(".section-objetivos");
 
 window.addEventListener("resize", () => {
-  const height = contentMain.clientHeight;
-  contentMain.style.bottom = `-${height / 2}px`;
-  sectionObjetivos.style.paddingTop = `${height / 2 + 36}px`;
-  console.log(`${height / 2}px`);
+  const heightContentMain = contentMain.clientHeight;
+  contentMain.style.bottom = `-${heightContentMain / 2}px`;
+  sectionObjetivos.style.paddingTop = `${heightContentMain / 2 + 36}px`;
 });
 
 window.addEventListener("load", () => {
-  const height = contentMain.clientHeight;
-  contentMain.style.bottom = `-${height / 2}px`;
-  sectionObjetivos.style.paddingTop = `${height / 2 + 36}px`;
-  console.log(`${height / 2}px`);
+  const heightContentMain = contentMain.clientHeight;
+  contentMain.style.bottom = `-${heightContentMain / 2}px`;
+  sectionObjetivos.style.paddingTop = `${heightContentMain / 2 + 36}px`;
 });
