f01a996da28eae9577c7684cc991e33ad2ad6256
Ramon Vieira <ramonvieira122333@gmail.com>
2022-09-06 09:43:51 -0300

    removing unnecessary lines to the end client

--- script/modules/backToTop.js
+++ script/modules/backToTop.js
@@ -2,10 +2,8 @@ export default function backToTop() {
   window.addEventListener("scroll", () => {
     if (scrollY > 800) {
       document.getElementById("backToTopButton").classList.add("show");
-      console.log("maior que 400px");
     } else {
       document.getElementById("backToTopButton").classList.remove("show");
-      console.log("menor que 400px");
     }
   });
 }
